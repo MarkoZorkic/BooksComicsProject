3b6351e07622931f0c268404c3193f62ba83813b
Marko Zorkic <markozorkic1@gmail.com>
2023-07-02 20:39:50 +0200

    Sedded database with more data

--- BookComicsWebApi/BookComicsWebApi/Migrations/20230702183643_ExtendedSeedData.Designer.cs
+++ BookComicsWebApi/BookComicsWebApi/Migrations/20230702183643_ExtendedSeedData.Designer.cs
@@ -4,6 +4,7 @@ using BookComicsWebApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookComicsWebApi.Migrations
 {
     [DbContext(typeof(BooksComicDbContext))]
-    partial class BooksComicDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230702183643_ExtendedSeedData")]
+    partial class ExtendedSeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
